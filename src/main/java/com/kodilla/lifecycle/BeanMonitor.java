6fa491e230390ae702b7109dcfbe63d2b05645b6
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-09-06 13:15:38 +0200

    lifecycle

---
+++ src/main/java/com/kodilla/lifecycle/BeanMonitor.java
@@ -0,0 +1,27 @@
+package com.kodilla.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanMonitor implements BeanPostProcessor {
+
+    private static Logger logger = LoggerFactory.getLogger(BeanMonitor.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws
+            BeansException {
+
+        System.out.println("Before initialization of bean: " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After initialization of bean: " + beanName);
+        logger.info("Created bean: "+ beanName);
+        return bean;
+    }
+
+}
--- src/test/java/com/kodilla/lifecycle/BeanLifecycleApplicationTests.java
+++ src/test/java/com/kodilla/lifecycle/BeanLifecycleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kodilla.beanlifecycle;
+package com.kodilla.lifecycle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
